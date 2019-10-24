71f966da383669f7d5ca9f132265bebb9467b19e
Amit Reddy <>
2019-10-24 18:31:25 +0530

    testing scan repo pcf

--- src/main/java/com/fristProject/articleRecomender/helloWorld/HelloWorldController.java
+++ src/main/java/com/fristProject/articleRecomender/helloWorld/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
-		return "Hello World pcf test";
+		return "Hello World pcf test scan repo";
 	}
 	
 	@GetMapping(path="/hello-world-bean")
